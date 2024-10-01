8448d9b0d7abd2f8235c3fcf0e06e16b71bef232
tohanici <tohanici@gmail.com>
2024-10-01 11:42:32 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        long currentWorldPopulation = 7811080815L;
+        int carsProductionThisYear = 55235358;
+        char firstLetterInAlphabet  = 'A';
+        boolean javaIsAmazing = true;
+        float pI = 3.14159F;
+
+        System.out.println("Current World Population: " + currentWorldPopulation);
+        System.out.println("Cars Production ThisYear: " + carsProductionThisYear);
+        System.out.println("First Letter in Alphabet: " + firstLetterInAlphabet);
+        System.out.println("Java IsAmazing: " + javaIsAmazing);
+        System.out.println("PI: " + pI);
+
+
+
+
+
+
+
+
+
+
+    }
+}
